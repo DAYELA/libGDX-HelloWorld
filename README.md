3667d188e7f440164badd5e068348a495ce4de65
Damien <damien.ayela@gmail.com>
2021-02-05 10:56:42 +0100

    :sparkles: publishing code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-libGDX HelloWorld
+[<img width="128px" src="https://cdn.dayela.fr/emotes/300/HI_01.png">](https://cdn.dayela.fr/emotes/300/HI_01.png)
+
+## libGDX HelloWord
+
+------------------------------------
+
+- Nom : libGDX HelloWord
+- Développeur(s) : DAYELA
+
+## Description
+WanelyFriends est le microservice de gestion des "friends" de Wanely
---
+++ desktop/src/fr/dayela/gdx/helloworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.dayela.gdx.helloworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.dayela.gdx.helloworld.HelloWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HelloWorld(), config);
+	}
+}
